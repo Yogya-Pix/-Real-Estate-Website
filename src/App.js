e5e56ead53fc7a51aebab5e696567bf6df4aae96
yogya <yogya.sharma192004@gmail.com>
2023-06-19 23:19:25 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Body from './components/body/body';
+// import Slider from './components/imageSlider/Slider';
+// import PropertyBtn from './components/propertyTypeButton/PropertyBtn';
+// import SignUp from './components/SignUpPage/SignUp';
 
 function App() {
   return (
-   <>
+    <>
    <Body/>
    </>
   );
